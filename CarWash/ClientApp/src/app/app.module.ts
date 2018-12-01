d365891ee1ad7c510b039b26dfb728587238b8f0
NPerederiy <nick.perederiy@gmail.com>
2018-12-01 03:51:53 +0200

    Design improvements and minor refactoring
    - Changed the style of displaying cards
    - Fixed issue of centering AppComponent
    - All unused components are disabled
    - Unused styles are deleted

--- CarWash/ClientApp/src/app/app.module.ts
+++ CarWash/ClientApp/src/app/app.module.ts
@@ -5,29 +5,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { ServiceListComponent } from './service-list/service-list.component';
-import { ServiceListItemComponent } from './service-list-item/service-list-item.component';
-import { CalendarCarouselComponent } from './calendar-carousel/calendar-carousel.component';
-import { CalendarCarouselItemComponent } from './calendar-carousel-item/calendar-carousel-item.component';
-import { BoxListComponent } from './box-list/box-list.component';
-import { BoxListItemComponent } from './box-list-item/box-list-item.component';
-import { TimeBarComponent } from './time-bar/time-bar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NavMenuComponent } from './nav-menu/nav-menu.component';
+// import { ServiceListComponent } from './service-list/service-list.component';
+// import { ServiceListItemComponent } from './service-list-item/service-list-item.component';
+// import { CalendarCarouselComponent } from './calendar-carousel/calendar-carousel.component';
+// import { CalendarCarouselItemComponent } from './calendar-carousel-item/calendar-carousel-item.component';
+// import { BoxListComponent } from './box-list/box-list.component';
+// import { BoxListItemComponent } from './box-list-item/box-list-item.component';
+// import { TimeBarComponent } from './time-bar/time-bar.component';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OptionListComponent } from './option-list/option-list.component';
 import { OptionListItemComponent } from './option-list-item/option-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    ServiceListComponent,
-    ServiceListItemComponent,
-    CalendarCarouselComponent,
-    CalendarCarouselItemComponent,
-    BoxListComponent,
-    BoxListItemComponent,
-    TimeBarComponent,
+    // NavMenuComponent,
+    // ServiceListComponent,
+    // ServiceListItemComponent,
+    // CalendarCarouselComponent,
+    // CalendarCarouselItemComponent,
+    // BoxListComponent,
+    // BoxListItemComponent,
+    // TimeBarComponent,
     OptionListComponent,
     OptionListItemComponent
   ],
@@ -35,7 +35,7 @@ import { OptionListItemComponent } from './option-list-item/option-list-item.com
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    NgbModule,
+    // NgbModule,
     RouterModule.forRoot([
       // { path: 'fetch-data', component: FetchDataComponent },
     ])
